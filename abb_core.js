57e9068742682fcb857b00876a2655a0315ac62c
Mymlogic.official <mymlogic.official@gmail.com>
2026-01-25 19:05:51 -0800

    Create abb_core.js
    Signed-off-by: Mymlogic.official <mymlogic.official@gmail.com>

---
+++ abb_core.js
@@ -0,0 +1,18 @@
+// SE CUSTOMS - ABB PREMIUM OVERRIDE
+const abbRefinement = {
+    vortex: {
+        material: "pearlescent_glass",
+        color: "hex_88CCFF", // Clear glassy blue
+        opacity: 0.65,
+        refraction: 1.2
+    },
+    chassis: {
+        edge_rounding: "5%", // Softened industrial feel
+        finish: "satin_metallic"
+    },
+    energy: {
+        pulse_sync: "dubstep_high_kick",
+        light_glow: "neon_cyan"
+    }
+};
+
