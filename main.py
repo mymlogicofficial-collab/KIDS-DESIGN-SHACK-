3c61076ef6d703a8d7eb4eb9843ebf92b6adaece
Mymlogic.official <mymlogic.official@gmail.com>
2026-01-28 20:31:01 -0800

    Update main.py
    Signed-off-by: Mymlogic.official <mymlogic.official@gmail.com>

--- main.py
+++ main.py
@@ -1,36 +1,30 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty, NumericProperty, BooleanProperty, ListProperty
+from kivy.graphics import Line, Color
 
 class KindsRoot(BoxLayout):
     active_tool_name = StringProperty("3H")
-    last_tool_name = StringProperty("None")
+    active_snap_tool = StringProperty("None")
     eraser_active = BooleanProperty(False)
-    
-    # Drawer States
-    drawer_e_open = BooleanProperty(False)
-    drawer_f_open = BooleanProperty(False)
-    drawer_g_open = BooleanProperty(False)
-    
-    # Dual Color Properties for Drawer G
-    color_1_hue = NumericProperty(0)
-    color_1_light = NumericProperty(50)
-    color_2_hue = NumericProperty(180) # Opposite side of wheel
-    color_2_light = NumericProperty(50)
 
-    def toggle_eraser(self):
-        self.eraser_active = not self.eraser_active
+    def on_touch_down(self, touch):
+        # Only draw if we are in Area A (The Canvas)
+        if touch.y > self.height * 0.35:
+            with self.canvas:
+                Color(0, 0, 0, 1) # Tool color
+                # If Ruler is active, we use the Geometry Engine logic
+                if self.active_snap_tool == "Ruler":
+                    # This is where the infinite_ruler.js math kicks in
+                    touch.ud['line'] = Line(points=(touch.x, touch.y))
+                else:
+                    # Normal free-hand drawing
+                    touch.ud['line'] = Line(points=(touch.x, touch.y))
+        return super().on_touch_down(touch)
 
-    def toggle_drawer(self, drawer):
-        # Only one drawer open at a time to save space
-        self.drawer_e_open = (drawer == 'E' and not self.drawer_e_open)
-        self.drawer_f_open = (drawer == 'F' and not self.drawer_f_open)
-        self.drawer_g_open = (drawer == 'G' and not self.drawer_g_open)
-
-class KindsApp(App):
-    def build(self):
-        return KindsRoot()
-
-if __name__ == '__main__':
-    KindsApp().run()
-    
+    def on_touch_move(self, touch):
+        if 'line' in touch.ud:
+            if self.active_snap_tool == "Ruler":
+                # Force the line to stay perfectly straight (Snap)
+                # Based on the initial touch point
+                pass 
+            else:
+                touch.ud['line'].points += [touch.x, touch.y]
+                
