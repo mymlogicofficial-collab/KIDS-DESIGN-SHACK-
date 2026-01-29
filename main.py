57af9c71d0b887132e289b27e785e43dc126037a
Mymlogic.official <mymlogic.official@gmail.com>
2026-01-28 19:57:44 -0800

    Update main.py
    Signed-off-by: Mymlogic.official <mymlogic.official@gmail.com>

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from kivy.properties import ListProperty
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, NumericProperty, BooleanProperty, ListProperty
 
 class KindsRoot(BoxLayout):
     active_lead = StringProperty("3H")
     active_soft = StringProperty("2B")
     active_tip = StringProperty("°")
+    active_tool_name = StringProperty("None") # Current tool from the grid
     
     # Drawer States
+    drawer_e_open = BooleanProperty(False)
     drawer_f_open = BooleanProperty(False)
-    drawer_g_open = BooleanProperty(False) # Added for Color Lab
+    drawer_g_open = BooleanProperty(False)
     
-    # Drawer F Properties
+    # Tool Properties
     tip_size = NumericProperty(10)
     tip_opacity = NumericProperty(100)
     tip_gression = NumericProperty(50)
-    
-    # Drawer G Properties (Color Lab)
-    main_hue = NumericProperty(0) # 0 to 360 degrees
-    light_blend = NumericProperty(0) # Slider 1
-    dark_blend = NumericProperty(0)  # Slider 2
-    current_color = ListProperty([1, 1, 1, 1]) # Resulting RGBA
+    current_color = ListProperty([1, 1, 1, 1])
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -32,19 +31,29 @@ class KindsRoot(BoxLayout):
     def cycle_h_lead(self):
         self.h_index = (self.h_index + 1) % len(self.h_leads)
         self.active_lead = self.h_leads[self.h_index]
+        self.active_tool_name = self.active_lead
 
     def cycle_b_lead(self):
         self.b_index = (self.b_index + 1) % len(self.b_leads)
         self.active_soft = self.b_leads[self.b_index]
+        self.active_tool_name = self.active_soft
 
     def cycle_tip_shape(self):
         self.tip_index = (self.tip_index + 1) % len(self.tip_shapes)
         self.active_tip = self.tip_shapes[self.tip_index]
 
-    def toggle_drawer_f(self):
-        self.drawer_f_open = not self.drawer_f_open
-        self.drawer_g_open = False # Close G if F opens
+    def select_tool(self, name):
+        self.active_tool_name = name
 
-    def toggle_drawer_g(self): # Added function
-        self.drawer_g_open = not self.drawer_g_open
-        self.drawer_f_open = False # Close F if G opens
+    def toggle_drawer(self, drawer):
+        self.drawer_e_open = (drawer == 'E' and not self.drawer_e_open)
+        self.drawer_f_open = (drawer == 'F' and not self.drawer_f_open)
+        self.drawer_g_open = (drawer == 'G' and not self.drawer_g_open)
+
+class KindsApp(App):
+    def build(self):
+        return KindsRoot()
+
+if __name__ == '__main__':
+    KindsApp().run()
+    
